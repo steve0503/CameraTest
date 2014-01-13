edec08e5b9f4e9aac94f014d4c3ac5439098b0b3
steve0503 <leehr76@gmail.com>
2014-01-14 08:15:32 +0900

    CameraTest

--- CameraTest/ViewController.m
+++ CameraTest/ViewController.m
@@ -10,15 +10,71 @@
 
 @interface ViewController ()
 
+
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+
 @end
 
 @implementation ViewController
+
+
+- (IBAction)takePicture:(id)sender {
+    
+    if (![UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]) {
+        //에러 처리
+        
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"오류" message:@"카메라가 지원되지 않는 기종입니다." delegate:nil cancelButtonTitle:@"확인" otherButtonTitles:nil];
+        [alert show];
+    }
+    
+    UIImagePickerController *imagePicker = [[UIImagePickerController alloc]init];
+    imagePicker.delegate = self;
+    imagePicker.allowsEditing = YES;
+    imagePicker.sourceType = UIImagePickerControllerSourceTypeCamera;
+    
+    [self presentModalViewController:imagePicker animated:YES];
+    
+}
+
+
+- (IBAction)getImage:(id)sender {
+    
+    UIImagePickerController *imagePicker = [[UIImagePickerController alloc]init];
+    
+    imagePicker.delegate = self;
+    
+    imagePicker.allowsEditing = YES;
+    
+    imagePicker.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+    
+    [self presentModalViewController:imagePicker animated:YES];
+    
+    
+    
+}
+
+-(void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info{
+    
+    UIImage *editImage = [info objectForKey:UIImagePickerControllerEditedImage];
+    
+    UIImage *originalImage = [info objectForKey:UIImagePickerControllerEditedImage];
+    
+    UIImage *usingImage = (nil == editImage)?originalImage:editImage;
+    
+    self.imageView.image = usingImage;
+    
+    [picker dismissModalViewControllerAnimated:YES];
+    
+}
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
 }
+
+
+
 
 - (void)didReceiveMemoryWarning
 {
